4beee5fd50d7b991283b11788e9280b0ab8d0d37
Abdullah Al Nomaan <83067643+SaikatNoman12@users.noreply.github.com>
2025-05-07 03:34:36 +0600

    learn how to validaton environmant variable

---
+++ src/config/env.validation.ts
@@ -0,0 +1,17 @@
+import * as Joi from 'joi';
+
+export default Joi.object({
+  NODE_ENV: Joi.string()
+    .valid('development', 'test', 'production')
+    .default('development'),
+  ENV_MODE: Joi.string().required(),
+  DB_TYPE: Joi.string().required(),
+  DB_HOST: Joi.string().required(),
+  DB_PORT: Joi.number().port().default(5432),
+  DB_USERNAME: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
+  DB_NAME: Joi.string().required(),
+  DB_AUTO_LOAD: Joi.boolean().required(),
+  SYNC: Joi.boolean().required(),
+  SECRET_KEY: Joi.string().required(),
+});
